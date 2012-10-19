20e8cbde2694c0b2d73d2152e358e839b1655fce
Oscar <oscarmeier@gmx.net>
2012-10-19 21:40:43 +0200

    imported some smileys for the comments.

--- trade/app/models/store/comment.rb
+++ trade/app/models/store/comment.rb
@@ -24,5 +24,16 @@ module Store
      def self.by_id(id)
        return @@comments[id]
      end
+
+    def format_description
+      formatted_desc = description.gsub(':)', '![alternative text](public/images/smile.gif)').gsub(':D', '![alternative text](public/images/laugh.gif)').
+          gsub(':(', '![alternative text](public/images/disappointed.gif)').gsub(':O', '![alternative text](public/images/shocked.gif)').
+          gsub(":'(", '![alternative text](public/images/sad.gif)').gsub(':/', '![alternative text](public/images/double_minded.gif)').
+          gsub('8)', '![alternative text](public/images/cool.gif)').gsub(':crazy:', '![alternative text](public/images/crazy.gif)').
+          gsub(':yeah:', '![alternative text](public/images/yeah.gif)')
+      return formatted_desc
+
+    end
+
   end
 end
