872d42eb5fe55bdbffa3116299ad856327007592
Urs Gerber <ug.gerber@gmail.com>
2012-10-20 22:19:33 +0200

    First working version of a context switcher. No functionality yet except user object stores organization it is working on behalf of
    Page is refreshed every time the user switches context
    
    	modified:   trade/app/app.rb
    	modified:   trade/app/controllers/user_ctrl.rb
    	modified:   trade/app/models/store/organization.rb
    	modified:   trade/app/models/store/user.rb
    	modified:   trade/app/public/styles/styles.less
    	modified:   trade/app/views/all_organizations.haml
    	modified:   trade/app/views/layout.haml
    	modified:   trade/app/views/profile.haml

--- trade/app/models/store/user.rb
+++ trade/app/models/store/user.rb
@@ -8,7 +8,7 @@ module Store
   class User < System_User
     @@users = {}
 
-    attr_accessor  :pwd_hash, :pwd_salt
+    attr_accessor  :pwd_hash, :pwd_salt, :on_behalf_of, :organizations
 
     def initialize
       self.name = ""
@@ -19,6 +19,8 @@ module Store
       self.description = ""
       self.open_item_page_time = Time.now
 	    self.image_path = "/images/no_image.gif"
+      self.on_behalf_of = self
+      self.organizations = []
     end
 
     def self.named(name)
@@ -63,13 +65,32 @@ module Store
       return user
     end
 
-
     def login
       Analytics::UserLoginActivity.with_username(name).log
     end
 
     def logout
       Analytics::UserLogoutActivity.with_username(name).log
+    end
+
+    # tell user to work on behalf of an organization
+    def work_on_behalf_of(organization)
+      self.on_behalf_of = organization
+    end
+
+    # become a member of an organization
+    def enter_organization(organization)
+      self.organizations << organization
+    end
+
+    # resign as a member of an organization
+    def leave_organization(organization)
+      self.organizations.delete organization
+    end
+
+    # return all organizations this user is a member of
+    def get_organizations
+      return self.organizations
     end
   end
 end
