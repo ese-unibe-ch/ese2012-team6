e4ab99f41a7d7f63f72832337dc47a9620f68446
Urs Gerber <ug.gerber@gmail.com>
2012-10-20 12:02:47 +0200

    Moved smileys into folder
    	modified:   trade/app/models/store/comment.rb
    	renamed:    trade/app/public/images/cool.gif -> trade/app/public/images/smileys/cool.gif
    	renamed:    trade/app/public/images/crazy.gif -> trade/app/public/images/smileys/crazy.gif
    	renamed:    trade/app/public/images/disappointed.gif -> trade/app/public/images/smileys/disappointed.gif
    	renamed:    trade/app/public/images/double_minded.gif -> trade/app/public/images/smileys/double_minded.gif
    	renamed:    trade/app/public/images/laugh.gif -> trade/app/public/images/smileys/laugh.gif
    	renamed:    trade/app/public/images/sad.gif -> trade/app/public/images/smileys/sad.gif
    	renamed:    trade/app/public/images/shocked.gif -> trade/app/public/images/smileys/shocked.gif
    	renamed:    trade/app/public/images/smile.gif -> trade/app/public/images/smileys/smile.gif
    	renamed:    trade/app/public/images/yeah.gif -> trade/app/public/images/smileys/yeah.gif

--- trade/app/models/store/comment.rb
+++ trade/app/models/store/comment.rb
@@ -26,11 +26,11 @@ module Store
      end
 
     def format_description
-      formatted_desc = description.gsub(':)', '![alternative text](public/images/smile.gif)').gsub(':D', '![alternative text](public/images/laugh.gif)').
-          gsub(':(', '![alternative text](public/images/disappointed.gif)').gsub(':O', '![alternative text](public/images/shocked.gif)').
-          gsub(":'(", '![alternative text](public/images/sad.gif)').gsub(':/', '![alternative text](public/images/double_minded.gif)').
-          gsub('8)', '![alternative text](public/images/cool.gif)').gsub(':crazy:', '![alternative text](public/images/crazy.gif)').
-          gsub(':yeah:', '![alternative text](public/images/yeah.gif)')
+      formatted_desc = description.gsub(':)', '![alternative text](/images/smileys/smile.gif)').gsub(':D', '![alternative text](/images/smileys/laugh.gif)').
+          gsub(':(', '![alternative text](/images/smileys/disappointed.gif)').gsub(':O', '![alternative text](/images/smileys/shocked.gif)').
+          gsub(":'(", '![alternative text](/images/smileys/sad.gif)').gsub(':/', '![alternative text](/images/smileys/double_minded.gif)').
+          gsub('8)', '![alternative text](/images/smileys/cool.gif)').gsub(':crazy:', '![alternative text](/images/smileys/crazy.gif)').
+          gsub(':yeah:', '![alternative text](/images/smileys/yeah.gif)')
       return formatted_desc
 
     end
