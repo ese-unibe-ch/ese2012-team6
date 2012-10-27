58dcbf092e1102c89de725912f7548ef055aa220
nachtigall-83 <nachtigall_83@yahoo.de>
2012-10-27 17:56:57 +0200

    Added a password generator

--- trade/app/models/security/password_generator.rb
+++ trade/app/models/security/password_generator.rb
@@ -1,5 +1,12 @@
+require 'rand62'
+
 module Security
   class PasswordGenerator
+
+    def generate_new_password
+      random_string = Rand62.safe(8)
+      user.pwd_hash = BCrypt::Engine.hash_secret(random_string, user.pwd_salt)
+    end
 
   end
 end
