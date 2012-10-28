a32f9ace6998038f75fd3e7d0511ada3b1fcfcea
Mansour Hamidi <mansourhamidi@hotmail.com>
2012-10-28 14:48:39 +0100

    system_user_test should be made

---
+++ trade/test/system_user_test.rb
@@ -0,0 +1,22 @@
+require "test/unit"
+require 'rubygems'
+require 'require_relative'
+require_relative '../app/models/store/system_user'
+require_relative '../app/models/store/item'
+require_relative '../app/models/store/user'
+require_relative '../app/models/store/organization'
+require_relative '../app/models/security/string_checker'
+
+class SystemUserTest < Test::Unit::TestCase
+
+  def setup
+
+  end
+
+  # Fake test
+  def test_fail
+
+    # To change this template use File | Settings | File Templates.
+    fail("Not implemented")
+  end
+end
