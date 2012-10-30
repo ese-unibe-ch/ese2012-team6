964feb22676c559dd50d0d776fc7dff1c16ce935
Urs Gerber <ug.gerber@gmail.com>
2012-10-31 00:21:57 +0100

    Removed gem rand62. Because that's how we roll...
    	modified:   Gemfile
    	modified:   Gemfile.lock
    	modified:   trade/app/models/security/password_generator.rb

--- trade/app/models/security/password_generator.rb
+++ trade/app/models/security/password_generator.rb
@@ -1,12 +1,9 @@
-require 'rubygems'
-require 'rand62'
-
 module Security
   # provides a method to generate save passwords
   class PasswordGenerator
-    # generate a new save password which is 8 characters long
+    # generate a new save password which is 16 characters long
     def self.generate_new_password
-      length=10
+      length = 16
       rand(36**length).to_s(36)
     end
   end
