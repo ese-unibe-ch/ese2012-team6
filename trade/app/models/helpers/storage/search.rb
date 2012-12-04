39b4d8c33eb1874fc951c445b30c5041601d291a
Urs Gerber <ug.gerber@gmail.com>
2012-12-04 11:17:11 +0100

    Added buy button to item popup in search. Search now excludes items in pending state
    	modified:   trade/app/models/helpers/storage/search.rb
    	modified:   trade/app/views/partials/item_popup.haml
    	modified:   trade/app/views/search_items.haml

--- trade/app/models/helpers/storage/search.rb
+++ trade/app/models/helpers/storage/search.rb
@@ -5,7 +5,7 @@ module Storage
     #Returns all items, which match the input of the user.
     def self.search(description)
 
-      items = Store::Item.all
+      items = Store::Item.all.select{|item| item.state != :pending}
       pattern = "(?i)(\w+)?(#{description})(\w+)?"
       begin
         return items.select {|x| x.description.match(pattern) or x.name.match(pattern)}
